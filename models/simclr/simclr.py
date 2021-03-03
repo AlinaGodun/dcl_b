a6aeb10789ee6978799372c3e6a42ffb03888be8
Alina Godun <godun.lina@gmail.com>
2021-03-03 11:49:57 +0100

    implement fit for simclr

--- models/simclr/simclr.py
+++ models/simclr/simclr.py
@@ -1,7 +1,8 @@
+import torch
 import torchvision
-from torch import nn
 import cifar_resnets
-
+from torch import nn
+from simple_contrastive_loss import SimpleContrastiveLoss
 
 class SimCLR(nn.Module):
     def __init__(self, output_dim, resnet_model='cifar_resnet'):
@@ -35,3 +36,30 @@ class SimCLR(nn.Module):
         feats = self.base_encoder(x)
         mapped_feats = self.projection_head(feats)
         return feats, mapped_feats
+
+    def fit(self, train_loader, epochs, start_lr, device, writer, tau=0.5, model_path=None, weight_decay=1e-5):
+        optimizer = torch.optim.Adam(self.parameters(), lr=start_lr) # weight_decay ?
+        loss = SimpleContrastiveLoss(tau)
+        i = 0
+        for epoch in epochs:
+            for step, ((x_i, x_j), _) in enumerate(train_loader):
+                i += 1
+                x_i = x_i.to(device)
+                x_j = x_j.to(device)
+
+                optimizer.zero_grad()
+
+                _, mapped_feats_i = self(x_i)
+                _, mapped_feats_j = self(x_j)
+
+                loss = loss(mapped_feats_i, mapped_feats_j)
+
+                loss.backward()
+                optimizer.step()
+
+                if epoch % 5 == 0 and model_path is not None:
+                    print(f"{self.name}: Epoch {epoch + 1}/{epochs} - Iteration {i} - Train loss:{loss.item():.4f},",
+                          f"LR: {optimizer.param_groups[0]['lr']}")
+                    torch.save(self.state_dict(), model_path)
+        return self
+
